ea0f6f28f73e31651e00749a72321acda2b9bec4
markos <markos.paltser@voco.ee>
2023-05-09 10:09:51 +0300

    harjutus 2

--- test.js
+++ test.js
@@ -4,3 +4,16 @@ var fs = require("fs");
 var filenames = fs.readdirSync('kolmapaev');
 console.log(filenames);
 console.log(filenames.length);
+var directoryPath = 'kolmapaev';
+var files = fs.readdirSync(directoryPath);
+var contents = [];
+for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
+    var file = files_1[_i];
+    if (file.endsWith('.txt')) {
+        var filePath = "".concat(directoryPath, "/").concat(file);
+        var fileContent = fs.readFileSync(filePath, 'utf-8');
+        contents.push(fileContent);
+        console.log("".concat(file, ": ").concat(fileContent.length, " characters"));
+    }
+}
+console.log('All file contents:', contents);
--- test.ts
+++ test.ts
@@ -1,7 +1,22 @@
 import * as fs from 'fs';
 
-let filenames:string[] = fs.readdirSync('kolmapaev');
+let filenames: string[] = fs.readdirSync('kolmapaev');
 console.log(filenames);
 console.log(filenames.length);
-// count all files in that start with a
 
+
+const directoryPath = 'kolmapaev';
+let files = fs.readdirSync(directoryPath);
+
+let contents: string[] = [];
+
+for (const file of files) {
+    if (file.endsWith('.txt')) {
+        let filePath = `${directoryPath}/${file}`;
+        let fileContent = fs.readFileSync(filePath, 'utf-8');
+        contents.push(fileContent);
+        console.log(`${file}: ${fileContent.length}`);
+    }
+}
+
+console.log('All file contents:', contents);
