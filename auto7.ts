b837ea8dfb3631094f5b37cccdaeb44dbe57f51f
markos <markos.paltser@voco.ee>
2023-05-10 09:22:33 +0300

    harjutus 7

---
+++ auto7.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+
+function display(path: string): void {
+    console.log(path);
+    if (fs.lstatSync(path).isDirectory()) {
+        for (let subpath of fs.readdirSync(path)) {
+            display(path + "/" + subpath);
+        }
+    }
+}
+function findCarInDirectory(path: string, carname: string): void {
+    const filenames = fs.readdirSync(path);
+    if (filenames.includes(carname)) {
+        console.log(`Found ${carname} in ${path}`);
+    }
+    for (const filename of filenames) {
+        const subpath = path + '/' + filename;
+        if (fs.lstatSync(subpath).isDirectory()) {
+            findCarInDirectory(subpath, carname);
+        }
+    }
+}
+
+display('teekond2');
+console.log('-------------------')
+
+findCarInDirectory('teekond2', '123ABC.txt');
+
+
+
